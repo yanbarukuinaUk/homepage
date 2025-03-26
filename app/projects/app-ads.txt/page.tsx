9f1c9f95c576d57dcef5d6597ba8f6b390ee28a5
MYCOMPUTER\fuxav <kotake.ukyou.t5@dc.tohoku.ac.jp>
2025-03-27 03:44:26 +0900

    ファイルpublic

---
+++ app/projects/app-ads.txt/page.tsx
@@ -0,0 +1,11 @@
+function Project1() {
+  return (
+    <Button asChild>
+      <Link href="/files/app-ads.txt" download>
+        ZIPファイルをダウンロード
+      </Link>
+    </Button>
+  );
+}
+
+export default project_1;
