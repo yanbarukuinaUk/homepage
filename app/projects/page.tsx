47a876620f1557178637c19f943dcf8949574935
MYCOMPUTER\fuxav <kotake.ukyou.t5@dc.tohoku.ac.jp>
2025-03-27 02:37:41 +0900

    page追加

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -44,7 +44,7 @@ export default function ProjectsPage() {
               </div>
               <div className="flex gap-4">
                 <Button asChild>
-                  <Link href={`/project/second`}>
+                  <Link href={`/projects/second`}>
                     プロジェクト詳細を見る
                   </Link>
                 </Button>
