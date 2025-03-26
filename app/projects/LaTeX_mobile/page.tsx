02b4824d0a6460b209194575c8362e4e97ff39d8
MYCOMPUTER\fuxav <kotake.ukyou.t5@dc.tohoku.ac.jp>
2025-03-27 03:29:40 +0900

    ここがポイント

--- app/projects/LaTeX_mobile/page.tsx
+++ app/projects/LaTeX_mobile/page.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Github, Twitter } from "lucide-react"
+import Link from "next/link"
+
 
 export default function LaTeX() {
   return (
