bb6dda6cb54e6a32768d157ada54dfacbe7bfe11
MYCOMPUTER\fuxav <kotake.ukyou.t5@dc.tohoku.ac.jp>
2025-03-26 10:05:57 +0900

    Deploy static site to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/homepage',  // GitHub Pagesのリポジトリ名に合わせて '/homepage' に設定
+  assetPrefix: '/homepage',  // 静的ファイルのパスを調整
   eslint: {
     ignoreDuringBuilds: true,
   },
